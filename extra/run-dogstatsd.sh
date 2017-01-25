9a0f558d278cef4a9732fdbbe025ff25ef1faa98
Paul Simpson <prsimp@gmail.com>
2017-01-24 22:58:56 -0600

    Rewrites agent script to unbreak console access

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -2,32 +2,39 @@
 
 if [[ $DISABLE_DATADOG_AGENT ]]; then
   echo "DISABLE_DATADOG_AGENT environment variable is set, not starting the agent."
-  exit 0
-fi
-
-if [[ $DATADOG_API_KEY ]]; then
-  sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  return 0
 else
-  echo "DATADOG_API_KEY environment variable not set. Run: heroku config:add DATADOG_API_KEY=<your API key>"
-  exit 1
-fi
+  successful_config=true
 
-if [[ $HEROKU_APP_NAME ]]; then
-  sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
-else
-  echo "HEROKU_APP_NAME environment variable not set. Run: heroku apps:info|grep ===|cut -d' ' -f2"
-  exit 1
-fi
+  if [[ $DATADOG_API_KEY ]]; then
+    sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  else
+    successful_config=false
+    echo "DATADOG_API_KEY environment variable not set. Run: heroku config:add DATADOG_API_KEY=<your API key>"
+  fi
 
-if [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
-  sed -i -e "s/^.*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
-fi
+  if [[ $HEROKU_APP_NAME ]]; then
+    sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  else
+    successful_config=false
+    echo "HEROKU_APP_NAME environment variable not set. Run: heroku apps:info|grep ===|cut -d' ' -f2"
+  fi
 
-(
-  # Unset other PYTHONPATH/PYTHONHOME variables before we start
-  unset PYTHONHOME PYTHONPATH
-  # Load our library path first when starting up
-  export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
-  mkdir -p /tmp/logs/datadog
-  exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
-)
+  if [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
+    sed -i -e "s/^.*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  fi
+
+  if [ "$successful_config" = true ]; then
+    (
+      # Unset other PYTHONPATH/PYTHONHOME variables before we start
+      unset PYTHONHOME PYTHONPATH
+      # Load our library path first when starting up
+      export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
+      mkdir -p /tmp/logs/datadog
+      exec /app/.apt/opt/datadog-agent/embedded/bin/python /app/.apt/opt/datadog-agent/agent/dogstatsd.py start
+    )
+  else
+    echo "Configuration issue, not starting the agent."
+    return 1
+  fi
+fi
